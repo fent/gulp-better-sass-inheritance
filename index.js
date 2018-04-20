f33910712525bb43809762ff186e799903fc4abd
fent <933490+fent@users.noreply.github.com>
2018-04-19 23:28:36 -0400

    feat: use modified-time from imports in parents

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
 var PLUGIN_NAME = 'gulp-better-sass-inheritance';
 
-var stream;
 
 function gulpBetterSassInheritance(options) {
 	options = options || {};
 
+	var stream;
 	var files = [];
 	var filesPaths = [];
 	var graph;
+	var modifiedTimeGraph;
 
 	if (!options.base) {
 		throw new PluginError(PLUGIN_NAME, 'Missing option `base`!');
@@ -51,9 +52,28 @@ function gulpBetterSassInheritance(options) {
 		return true;
 	}
 
+	function getLastModifiedTime() {
+		var map = {};
+		for (var filepath in graph.index) {
+			var node = graph.index[filepath];
+			var parentsModified = getModified(node, 'importedBy');
+			var childrenModified = getModified(node, 'imports');
+			map[filepath] = new Date(Math.max(node.modified, parentsModified, childrenModified))
+		}
+		return map;
+	}
+
+	function getModified(node, key) {
+		return Math.max.apply(null, node[key].map(function(filepath) {
+			var node = graph.index[filepath];
+			return node ? Math.max(node.modified, getModified(node, key)) : 0;
+		}));
+	}
+
 	function endStream() {
 		if (files.length) {
 			graph = sassGraph.parseDir(options.base, options);
+			modifiedTimeGraph = getLastModifiedTime(graph);
 
 			check(_.map(files, function (item) {
 				return item.path;
@@ -62,6 +82,10 @@ function gulpBetterSassInheritance(options) {
 			vfs.src(filesPaths, {'base': options.base})
 				.pipe(es.through(
 					function (f) {
+						if (modifiedTimeGraph[f.path]) {
+							f.stat.mtime = modifiedTimeGraph[f.path];
+							f.stat.mtimeMs = modifiedTimeGraph[f.path].getTime();
+						}
 						stream.emit('data', f);
 					},
 					function () {
